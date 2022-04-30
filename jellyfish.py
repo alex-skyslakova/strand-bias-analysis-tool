53de6205ee8c37bdaea609cf3c26672b9a2d35a1
Alexandra Skyslakova <xskyslak@fi.muni.cz>
2022-04-30 13:51:22 +0200

    Refactoring connected to split into classes - Jellyfish

--- jellyfish.py
+++ jellyfish.py
@@ -8,13 +8,17 @@ class Jellyfish:
     def __init__(self, threads=1, hash_size="500M", output_dir=''):
         self.threads = threads
         self.hash_size = hash_size
+        self.jf_dir = self.set_outdir(output_dir)
+
+
+    def set_outdir(self, output_dir):
         self.jf_dir = os.path.join(output_dir, 'sbat', 'jellyfish')
         utils.is_or_create_dir(self.jf_dir)
 
     def run_jellyfish(self, input_file, k):
         dump_file = os.path.join(self.jf_dir, "mer_counts.jf")
         calculate = "jellyfish count -m {0} -s {1} -t {2} -o {3} {4}".format(k, self.hash_size, self.threads,
-                                                                             self.jf_dir, input_file)
+                                                                             dump_file, input_file)
         print(calculate)
         subprocess.run(calculate.split(" "), stdout=subprocess.PIPE)
         dump = "jellyfish dump {0}".format(dump_file)
