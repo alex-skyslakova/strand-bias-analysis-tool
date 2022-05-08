ec2395cc8f35f45a26ec16eefe73856bebfd40e1
Alexandra Skyslakova <xskyslak@fi.muni.cz>
2022-05-08 13:11:08 +0200

    Rename src folder

--- setup.py
+++ setup.py
@@ -1,26 +1,46 @@
+import subprocess
+import sys
 import setuptools
+from setuptools.command.install import install
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+class Install(install):
+    """Customized setuptools install command - starts Makefile ensuring Jellyfish presence."""
+    def run(self):
+        jf_command_unix = ["make", "jellyfish"]
+        jf_command_mac = ["make", "jellyfish-mac"]
+        if subprocess.call(jf_command_unix) != 0:
+            sys.exit(-1)
+        install.run(self)
+
+
 setuptools.setup(
-    name="example-package-YOUR-USERNAME-HERE",
-    version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    name="sbat-alex_skyslakova",
+    version="0.0.3",
+    author="Alexandra Skyslakova",
+    author_email="alexandra.skyslakova@gmail.com",
+    description="A tool for strand bias analysis of NGS data",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
-    },
+    url="https://github.com/alex-skyslakova/strand-bias-analysis-tool",
+    #project_urls={
+    #    "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    #},
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Operating System :: MacOS",
+        "Operating System :: Unix",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
+    package_dir={"": "sbat"},
+    packages=setuptools.find_packages(where="sbat"),
+    python_requires=">=3.7",
+    entry_points={
+        'console_scripts': [
+            'sbat=sbat.main:main'
+        ]
+    },
 )
