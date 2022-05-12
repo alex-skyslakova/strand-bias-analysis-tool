d8e3a15ae52c6f79fb6ce59422c0ef3254354a8a
Alexandra Skyslakova <xskyslak@fi.muni.cz>
2022-05-12 13:47:00 +0200

    Fix setup.py and requirements

--- setup.py
+++ setup.py
@@ -1,33 +1,26 @@
-import subprocess
-import sys
 import setuptools
-from setuptools.command.install import install
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-class Install(install):
-    """Customized setuptools install command - starts Makefile ensuring Jellyfish presence."""
-    def run(self):
-        jf_command_unix = ["make", "jellyfish"]
-        jf_command_mac = ["make", "jellyfish-mac"]
-        if subprocess.call(jf_command_unix) != 0:
-            sys.exit(-1)
-        install.run(self)
-
-
 setuptools.setup(
     name="sbat-alex_skyslakova",
-    version="0.0.3",
+    version="0.0.5",
     author="Alexandra Skyslakova",
     author_email="alexandra.skyslakova@gmail.com",
     description="A tool for strand bias analysis of NGS data",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/alex-skyslakova/strand-bias-analysis-tool",
-    #project_urls={
-    #    "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
-    #},
+    install_requires=[
+        'Bio==1.3.8',
+        'matplotlib==3.5.2',
+        'numpy==1.22.3',
+        'pandas==1.4.2',
+        'python_dateutil==2.8.2',
+        'pytz==2022.1',
+        'setuptools==45.2.0'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
@@ -35,12 +28,11 @@ setuptools.setup(
         "Operating System :: MacOS",
         "Operating System :: Unix",
     ],
-    package_dir={"": "sbat"},
-    packages=setuptools.find_packages(where="sbat"),
+    packages=setuptools.find_packages(),
     python_requires=">=3.7",
     entry_points={
         'console_scripts': [
-            'sbat=sbat.main:main'
-        ]
-    },
+            'sbat=sbat.main:main',
+        ],
+    }
 )
